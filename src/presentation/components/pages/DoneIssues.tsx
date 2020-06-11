7532b893fcefc9c382db6de0ed7f8b074bb1ed7d
Daniel Caris <daniel.caris22@gmail.com>
2020-06-11 11:36:34 -0400

    :sparkles: Change labels to render on DoneIssues

--- src/presentation/components/pages/DoneIssues.tsx
+++ src/presentation/components/pages/DoneIssues.tsx
@@ -1,30 +1,46 @@
 import React, { useState } from 'react';
 import useFetchIssues from '../../../infrastructure/hooks/useFetchIssues';
 import Issue from '../../../domain/models/Issue';
-import MergeRequest, { MergeRequestStatus } from '../../../domain/models/MergeRequest';
+import MergeRequest, {
+  MergeRequestStatus,
+} from '../../../domain/models/MergeRequest';
 
 const DoneIssues = () => {
   const [labels] = useState(['Done By Dev']);
   const [issues, isLoading] = useFetchIssues(labels);
-  const labelsToShow = ['Bug', 'Feature', 'Enhancement', 'CC'];
+
+  const labelsWithDescriptionToShow = [
+    { label: 'type:: Bug', description: 'Fix' },
+    { label: 'type:: Feature', description: 'Feature' },
+    { label: 'type:: Enhancement', description: 'Mejoras' },
+    { label: 'type:: CC', description: 'Control de Cambios' },
+  ];
 
   const filterIssuesWithoutLabels = (labels: Array<string>) => {
     return issues.filter((issue: Issue) => {
-      return !labels.find(label => issue.labels && issue.labels.indexOf(label) !== -1);
+      return !labels.find(
+        (label) => issue.labels && issue.labels.indexOf(label) !== -1
+      );
     });
   };
 
   const isMergedMergeRequests = (mergeRequests: MergeRequest[]) => {
-    return mergeRequests.length > 0 && mergeRequests
-      .filter(
-        mergeRequest => mergeRequest.mergeStatus === MergeRequestStatus.merged
-      ).length === mergeRequests.length;
+    return (
+      mergeRequests.length > 0 &&
+      mergeRequests.filter(
+        (mergeRequest) => mergeRequest.mergeStatus === MergeRequestStatus.merged
+      ).length === mergeRequests.length
+    );
   };
 
   const renderIssuesByLabel = (label: string | null, title: string) => {
-    const filterIssues = label ? issues.filter(
-      ({ labels }: Issue) => labels && labels.indexOf(label) !== -1
-    ) : filterIssuesWithoutLabels(labelsToShow);
+    const filterIssues = label
+      ? issues.filter(
+          ({ labels }: Issue) => labels && labels.indexOf(label) !== -1
+        )
+      : filterIssuesWithoutLabels(
+          labelsWithDescriptionToShow.map(({ label }) => label)
+        );
 
     if (filterIssues.length === 0) {
       return null;
@@ -33,16 +49,19 @@ const DoneIssues = () => {
     return (
       <div style={{ marginBottom: 20 }}>
         <div>{title}:</div>
-        <br/>
+        <br />
         {filterIssues.map(({ iid, title, relatedMergeRequests }) => (
           <div key={iid}>
-            * #{iid} - {title} ({isMergedMergeRequests(relatedMergeRequests || []) ? 'Merged': 'Pending'})
+            * #{iid} - {title} (
+            {isMergedMergeRequests(relatedMergeRequests || [])
+              ? 'Merged'
+              : 'Pending'}
+            )
           </div>
         ))}
       </div>
     );
   };
-
 
   if (isLoading) {
     return <section>Cargando...</section>;
@@ -51,11 +70,10 @@ const DoneIssues = () => {
   return (
     <section>
       <section>Issues: {issues.length}</section>
-      <br/>
-      {renderIssuesByLabel('Bug', 'Fix')}
-      {renderIssuesByLabel('Feature', 'Feature')}
-      {renderIssuesByLabel('Enhancement', 'Mejoras')}
-      {renderIssuesByLabel('CC', 'Control de Cambios')}
+      <br />
+      {labelsWithDescriptionToShow.map(({ label, description }) =>
+        renderIssuesByLabel(label, description)
+      )}
       {renderIssuesByLabel(null, 'Otros')}
     </section>
   );
